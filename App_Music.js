c4fa6a98fd0dc0fd0777f8190655c73db611cd7f
PhamTuan <phamtuan19hd@gmail.com>
2023-03-09 00:14:50 +0700

    Play music

--- App_Music.js
+++ App_Music.js
@@ -1,24 +1,17 @@
 
 import song from "./song_list.js";
 
-
-
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-const app = {
+var currenIndex = 0;
 
-    currenIndex: 0,
-    isPlaying: false,
+function render() {
+    renderAudio();
 
-
-    // add bài hát vào danh sách phát nhạc
-    render: function () {
-        this.renderAudio();
-
-        const html = song.map(function (a) {
-            return ` 
-            <div class="song active__hover">
+    const html = song.map(function (a, index) {
+        return ` 
+            <div class="song active__hover ${index == 0 ? 'active' : ''}">
                 <div class="thumb" style="background-image: url('${a.image}')"></div>
                 <div class="body">
                     <h3 class="title">${a.name}</h3>
@@ -29,204 +22,202 @@ const app = {
                 </div>
             </div> 
             `
-        })
-        $('.playlist').innerHTML = html.join('')
-        this.clickSong();
-    },
+    })
 
-    renderAudio: function () {
-        $(".name__song").innerText = song[this.currenIndex].name;
-        $("#audio").src = song[this.currenIndex].path;
-        $(".cd-thumb").style.backgroundImage = "url('" + song[this.currenIndex].image + "')";
-    },
+    $('.playlist').innerHTML = html.join('')
+    clickSong();
+}
 
-    // xử lý khi vuốt đanh sách bài hát (thu nhỏ hình ảnh (class="cd"))
-    handlenEvents: function () {
-        const cd = $('.cd')
-        const cdWidth = cd.offsetWidth // offsetWidth: chiều rộng của class="cd
+// hiển thị bài hát cbi phát
+function renderAudio() {
+    $(".name__song").innerText = song[currenIndex].name;
+    $("#audio").src = song[currenIndex].path;
+    $(".cd-thumb").style.backgroundImage = "url('" + song[currenIndex].image + "')";
+}
 
-        this.render();
-        this.cdPlaying();
-        this.clickProgress();
-        this.cdReplay();
-        this.prevSong();
-        this.nextSong();
+// xử lý khi vuốt đanh sách bài hát (thu nhỏ hình ảnh (class="cd"))
+function handlenEvents() {
+    const cd = $('.cd')
+    const cdWidth = cd.offsetWidth // offsetWidth: chiều rộng của class="cd
 
-        // console.log(cdWidth);
-        document.onscroll = function () {
-            const onscrollTop = window.scrollY;
-            // kích thước mới = kích thước của hình ảnh - tỉ lẹ kéo thả
-            const newCdWidth = cdWidth - onscrollTop
+    document.onscroll = function () {
+        const onscrollTop = window.scrollY;
+        // kích thước mới = kích thước của hình ảnh - tỉ lẹ kéo thả
+        const newCdWidth = cdWidth - onscrollTop
 
-            cd.style.width = newCdWidth > 0 ? newCdWidth + 'px' : 0
-            // opacity = kích thức mới chia kích thước cũ
-            cd.style.opacity = newCdWidth / cdWidth
+        cd.style.width = newCdWidth > 0 ? newCdWidth + 'px' : 0
+        // opacity = kích thức mới chia kích thước cũ
+        cd.style.opacity = newCdWidth / cdWidth
+    }
+}
 
+// phát nhạc 
+function cdPlaying() {
+    $(".btn-toggle-play").onclick = () => {
+        // Check audio play 
+        if ($("#audio").paused) {
+            playSong();
+            $(".cd-thumb").classList.add('spin');
+        } else {
+            pauseSong();
+            $(".cd-thumb").classList.remove('spin');
         }
-    },
+    }
+}
 
-    cdPlaying: function () {
-        $(".btn-toggle-play").onclick = () => {
-            // Check audio play 
-            if ($("#audio").paused) {
-                this.playSong();
-                console.log($("#audio").duration);
-            } else {
-                this.pauseSong();
-            }
+// phát nhạc
+function playSong() {
+    playBtn();
+
+    $("#audio").play()
+
+    // Thời gian tổng bài hát .duration || thời gian đã phát .currentTime
+    $("#audio").addEventListener("timeupdate", () => {
+        const currentTime = $("#audio").currentTime;
+        const totalTime = $("#audio").duration;
+
+        const newPercentWidth = currentTime / totalTime * 100;
+        $(".progress-value").style.width = newPercentWidth + "%";
+
+        //  Cd-thumb xoay 360
+        // const style = window.getComputedStyle($(".cd-thumb"));
+        // const matrix = new DOMMatrixReadOnly(style.transform);
+        // const angle = Math.atan2(matrix.b, matrix.a) * (180 / Math.PI);
+        // $(".cd-thumb").style.transform = `rotate(${angle}deg)`;
+
+        cdAutoNext(newPercentWidth);
+    });
+}
+
+// dừng phát nhạc
+function pauseSong() {
+    stopBtn()
+    $("#audio").pause();
+}
+
+// phát lại
+function cdReplay() {
+    $(".btn-repeat").onclick = () => {
+        $("#audio").currentTime = 0;
+    }
+}
+
+// tùy chọn thời gian phát
+function clickProgress() {
+    $(".progress").onclick = (e) => {
+        const rect = $(".progress").getBoundingClientRect();
+        const widthClick = e.clientX - rect.left;
+        const widthProgress = widthClick / rect.width * 100;
+
+        // Hiển thị thời lượng đã phát
+        $(".progress-value").style.width = widthProgress + "%";
+
+        // tùy chọn thời gian phát
+        $("#audio").currentTime = $("#audio").duration * widthProgress / 100;
+        console.log(widthProgress);
+        if (widthProgress >= 99.64285578046527) {
+            stopBtn();
         }
-    },
+    }
+}
 
-    playSong: function () {
-        this.playBtn();
+function clickSong() {
+    $$(".song").forEach((e, index) => {
+        e.onclick = () => {
+            currenIndex = index;
 
-        $("#audio").play()
+            $$(".song").forEach((item) => {
+                item.classList.add("active__hover")
+                item.classList.remove("active");
+            })
 
-        // Thời gian tổng bài hát .duration || thời gian đã phát .currentTime
-        $("#audio").addEventListener("timeupdate", () => {
-            const newPercentWidth = $("#audio").currentTime / $("#audio").duration * 100;
-            $(".progress-value").style.width = newPercentWidth + "%";
-            // console.log(newPercentWidth);
+            e.classList.add("active");
+            e.classList.remove("active__hover");
 
-            this.cdAutoNext(newPercentWidth);
-        });
-
-    },
-
-    cdReplay: function () {
-        $(".btn-repeat").onclick = () => {
-            $("#audio").currentTime = 0;
+            stopBtn();
+            renderAudio();
         }
-    },
+    });
+}
 
-    pauseSong: function () {
-        this.stopBtn()
+function playBtn() {
+    $(".icon-play").classList.add('d-none');
+    $(".icon-pause").classList.remove('d-none');
+}
 
-        $("#audio").pause();
+function stopBtn() {
+    $(".icon-play").classList.remove('d-none');
+    $(".icon-pause").classList.add('d-none');
+}
 
-        // this.cdThumbAnimate().stop();
-    },
 
-    // tùy chọn thời gian phát
-    clickProgress: function () {
-        $(".progress").onclick = (e) => {
-            const rect = $(".progress").getBoundingClientRect();
-            const widthClick = e.clientX - rect.left;
-            const widthProgress = widthClick / rect.width * 100;
+function cdAutoNext(newPercentWidth) {
+    if (newPercentWidth == 100) {
+        if ($(".btn-random .active")) {
 
-            // Hiển thị thời lượng đã phát
-            $(".progress-value").style.width = widthProgress + "%";
+            currenIndex > song.length - 1 ? currenIndex = 0 : currenIndex++;
 
-            // tùy chọn thời gian phát
-            $("#audio").currentTime = $("#audio").duration * widthProgress / 100;
-            console.log(widthProgress);
-            if (widthProgress >= 99.64285578046527) {
-                this.stopBtn();
-            }
+            renderAudio();
+            $("#audio").play();
+            console.log(true);
+        } else {
+            stopBtn();
         }
-    },
+    }
+}
 
-    clickSong: function () {
+function prevSong() {
+    $(".btn-prev").onclick = () => {
+        currenIndex--;
+
+        if (currenIndex < 0) {
+            currenIndex = song.length - 1;
+        }
+
+        $("#audio").currentTime = 0;
+        $(".progress-value").style.width = 0;
+
+        renderAudio();
+        playSong();
+
         $$(".song").forEach((e, index) => {
-            e.onclick = () => {
-                $$(".song").forEach((item) => {
-                    item.classList.add("active__hover")
-                    item.classList.remove("active");
-                })
-
-                e.classList.add("active");
-                e.classList.remove("active__hover")
-                this.currenIndex = index;
-                this.stopBtn();
-                this.renderAudio();
-            }
+            e.classList.add("active__hover");
+            e.classList.remove("active");
         });
-    },
 
-    // animation speed cd-thumb
-    // cdThumbAnimate: function () {
-    //     const cdThumbAnimate = $(".cd-thumb").animate([
-    //         { transform: 'rotate(360deg)' }
-    //     ], {
-    //         duration: 10000,
-    //         iterations: Infinity,
-    //     })
-
-    //     const act = {
-    //         play() {
-    //             cdThumbAnimate.play();
-    //         },
-
-    //         stop() {
-    //             cdThumbAnimate.pause();
-    //         },
-    //     }
-
-    //     return act;
-    // },
-
-    prevSong: function () {
-        $(".btn-prev").onclick = () => {
-
-            this.currenIndex--;
-
-            if (this.currenIndex < 0) {
-                this.currenIndex = song.length - 1;
-            }
-
-            $("#audio").currentTime = 0;
-            $(".progress-value").style.width = 0;
-
-            this.stopBtn();
-            this.renderAudio();
-        }
-    },
-
-    nextSong: function () {
-        $(".btn-next").onclick = () => {
-            this.currenIndex++;
-
-            if (this.currenIndex == song.length - 1) {
-                this.currenIndex = 0;
-            }
-            $("#audio").currentTime = 0;
-            $(".progress-value").style.width = 0;
-
-            this.stopBtn();
-            this.renderAudio();
-        }
-    },
-
-    playBtn: function () {
-        $(".icon-play").classList.add('d-none');
-        $(".icon-pause").classList.remove('d-none');
-    },
-
-    stopBtn: function () {
-        $(".icon-play").classList.remove('d-none');
-        $(".icon-pause").classList.add('d-none');
-    },
-
-
-    cdAutoNext: function (newPercentWidth) {
-        if (newPercentWidth == 100) {
-            if ($(".btn-random .active")) {
-                this.currenIndex++;
-                this.renderAudio();
-                $("#audio").play();
-                console.log(true);
-            } else {
-                this.stopBtn();
-            }
-        }
-    },
-
-    start: function () {
-        this.handlenEvents();
-    },
+        $$(".song")[currenIndex].classList.remove("active__hover");
+        $$(".song")[currenIndex].classList.add("active");
+    }
 }
-app.start();
 
-$(".btn-random").onclick = () => {
-    $(".fa-random").classList.toggle("active");
+function nextSong() {
+    $(".btn-next").onclick = () => {
+        currenIndex++;
+
+        if (currenIndex == song.length - 1) {
+            currenIndex = 0;
+        }
+        $("#audio").currentTime = 0;
+        $(".progress-value").style.width = 0;
+
+        renderAudio();
+        playSong();
+
+        $$(".song").forEach((e, index) => {
+            e.classList.add("active__hover");
+            e.classList.remove("active");
+        });
+
+        $$(".song")[currenIndex].classList.remove("active__hover");
+        $$(".song")[currenIndex].classList.add("active");
+
+    }
 }
+
+handlenEvents()
+render();
+cdPlaying();
+clickProgress();
+cdReplay();
+prevSong();
+nextSong();
